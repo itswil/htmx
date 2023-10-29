26905bef321de7471f4cc19ec3f67fb4d3f453c6
itswil <ww8668@gmail.com>
2023-10-29 22:24:13 +0800

    Add fetch to About page

--- src/controllers/pages.tsx
+++ src/controllers/pages.tsx
@@ -8,4 +8,8 @@ export const pagesController = (app: Elysia) =>
   app
     .use(html())
     .get("/", ({ html }) => html(<Home />))
-    .get("/about", ({ html }) => html(<About name="haha" />));
+    .get("/about", async ({ html }) => {
+      const response = await fetch("https://rickandmortyapi.com/api/character");
+      const data = await response.json();
+      return html(<About name="haha" data={data} />);
+    });
--- src/templates/pages/About.tsx
+++ src/templates/pages/About.tsx
@@ -2,8 +2,14 @@ import { Main } from "../layouts/Main";
 
 type AboutParams = {
   name: string;
+  data: any; // yeeee
 };
 
-export const About = ({ name }: AboutParams) => {
-  return <Main>ABOUT: {name}</Main>;
+export const About = ({ name, data }: AboutParams) => {
+  return (
+    <Main>
+      <div>ABOUT: {name}</div>
+      <div>{JSON.stringify(data)}</div>
+    </Main>
+  );
 };
