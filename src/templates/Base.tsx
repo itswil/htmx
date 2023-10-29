30a69ecc7df222c5e64aa4f3209b8fcb32228a50
itswil <ww8668@gmail.com>
2023-10-29 22:06:52 +0800

    Update base title

--- src/templates/Base.tsx
+++ src/templates/Base.tsx
@@ -9,7 +9,7 @@ export const Base = ({ children }: Html.PropsWithChildren) => (
         <link rel="icon" type="image/jpg" href="/public/favicon.jpg" />
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Bun, Elysia, HTMX</title>
+        <title>Trying out HTMX</title>
       </head>
       <body>{children}</body>
     </html>
